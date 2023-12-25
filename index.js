38d5efd386dbbed753e87339b1ed403b026bb02e
MalikAhsan-PC <malikahsan13@gmail.com>
2023-12-26 00:53:59 +0500

    added endpoints for adding and updating course with Joi Validation mechanism

--- index.js
+++ index.js
@@ -1,10 +1,70 @@
+require("dotenv").config();
+const { parse } = require("dotenv");
 const express = require("express");
+const Joi = require("joi");
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const courses = [
+  { id: 1, name: "Course 1" },
+  { id: 2, name: "Course 2" },
+  { id: 3, name: "Course 3" },
+];
 
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
-app.listen("5000", () => {
-  console.log("Server listening on PORT 5000");
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+app.get("/api/course/:id", (req, res) => {
+  let course = courses.find((c) => c.id === parseInt(req.params.id));
+  res.send(course);
+});
+
+app.post("/api/course", (req, res) => {
+  const new_course = {
+    id: courses[courses.length - 1]?.id + 1 || 1,
+    name: req.body.name,
+  };
+  let result = validateInput(new_course);
+  if (result.error) {
+    res.status(400).json({ message: result.error.details[0].message });
+  } else {
+    courses.push(new_course);
+    res.send(courses);
+  }
+});
+
+app.put("/api/course/:id", (req, res) => {
+  let course = courses.find((course) => course.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(400).json({ message: "Course is not available" });
+  } else {
+    let { error } = validateInput(req.body);
+    if (error) {
+      res.status(400).json({ message: error.details[0].message });
+    } else {
+      course.name = req.body.name;
+      res.status(200).send(course);
+    }
+  }
+});
+
+function validateInput(course) {
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    // id: Joi.number().required(),
+  });
+  return schema.validate(course);
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server listening on PORT ${port}`);
 });
